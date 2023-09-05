7100e0c37dae901a40f773b843b9988e0a608d44
pro <pro@prodeMacBook-Pro-2.local>
2023-09-06 02:12:27 +0600

    update and delete with genric class view

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,16 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 # Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register(r'books', views.BookListView, basename="book")
+# router = DefaultRouter()
+# router.register(r'books', views.BookListView, basename="book")
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('books/', views.BookListView.as_view()),  # get,post request handle
+    # path('books/', views.BookListView.as_view()),  # get,post request handle
+    # # get,post request handle
+    # path('books/<int:pk>/', views.BookListUpdateDelete.as_view()),
     # get,post request handle
-    path('books/<int:pk>/', views.BookListUpdateDelete.as_view()),
+    path('books/', views.BookListCreateAPIView.as_view()),
+    # get,post request handle
+    path('books/<int:pk>/', views.BookRetrieveUpdateDestroyAPIView.as_view()),
 ]
