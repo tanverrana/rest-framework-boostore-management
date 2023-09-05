d11bc585673f815c409ebfefc3ffcff24bea2733
pro <pro@prodeMacBook-Pro-2.local>
2023-09-06 01:01:31 +0600

    add serializer in views

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import BookStoreModel
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class BookStoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookStoreModel
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .models import BookStoreModel
+from .serializers import BookStoreSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class SnippetList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+
+    def get(self, request, format=None):
+        model = BookStoreModel.objects.all()
+        serializer = BookStoreSerializer(model, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BookStoreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
